1bd904e1a963eb11bafd9b6e6777b45c8e63e9d4
Henrique-Hoi-Dev <riqueah@gmail.com>
2020-09-21 15:11:27 -0300

    Updates

---
+++ precache-manifest.87c5e248dc957c796838d343e1860c8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c25959e6012c82efb09102420731ce4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ca72da5ffff6fafe9374",
+    "url": "/portfolio/static/css/main.0c9895d5.chunk.css"
+  },
+  {
+    "revision": "91705cc66b7ab7e7dc4a",
+    "url": "/portfolio/static/js/2.52b5304c.chunk.js"
+  },
+  {
+    "revision": "768248413cf74f1cffab91faed2680c9",
+    "url": "/portfolio/static/js/2.52b5304c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca72da5ffff6fafe9374",
+    "url": "/portfolio/static/js/main.679d2a7f.chunk.js"
+  },
+  {
+    "revision": "d49fe173965a802803e2",
+    "url": "/portfolio/static/js/runtime-main.e6c30b28.js"
+  },
+  {
+    "revision": "c774d900e223b1679daccd4c2ccddea5",
+    "url": "/portfolio/static/media/img6.c774d900.jpg"
+  }
+]);
