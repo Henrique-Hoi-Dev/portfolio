d3b798a05910b81f2d522ae1922884937862e28c
Henrique-Hoi-Dev <riqueah@gmail.com>
2020-09-22 17:59:45 -0300

    Updates

--- precache-manifest.1a63b6ef71d0d8ec070334c894400507.js
+++ precache-manifest.1a63b6ef71d0d8ec070334c894400507.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "519da6f2a5510d6be55c3af98d1b243b",
+    "revision": "f2a5e97bf1483e9c720ca4525cde0c13",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "fa23dfcf2c7cc42171e4",
-    "url": "/portfolio/static/js/2.2a3aff7a.chunk.js"
+    "revision": "b5e5cd8fa73f00aa8f7f",
+    "url": "/portfolio/static/js/2.4a9fd633.chunk.js"
   },
   {
     "revision": "768248413cf74f1cffab91faed2680c9",
-    "url": "/portfolio/static/js/2.2a3aff7a.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.4a9fd633.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4f4d17f94906ace4a2cb",
-    "url": "/portfolio/static/js/main.3e43f535.chunk.js"
+    "revision": "d2f23a36fe046a6034f7",
+    "url": "/portfolio/static/js/main.dc737abc.chunk.js"
   },
   {
     "revision": "d49fe173965a802803e2",
@@ -26,5 +26,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "7dcfabfab769ad7d489b429d8a134612",
     "url": "/portfolio/static/media/background.7dcfabfa.jpg"
+  },
+  {
+    "revision": "dc5dd6cb1f9c4c8402752fd13fa44764",
+    "url": "/portfolio/static/media/user2.dc5dd6cb.png"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7729db7da47141e89fbc104fb9621eda.js"
+  "/portfolio/precache-manifest.1a63b6ef71d0d8ec070334c894400507.js"
 );
 
 self.addEventListener('message', (event) => {
