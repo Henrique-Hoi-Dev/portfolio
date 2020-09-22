74b52e17f675aef5275c75b99a9fccd6b7f868b3
Henrique-Hoi-Dev <riqueah@gmail.com>
2020-09-22 18:01:56 -0300

    Updates

---
+++ precache-manifest.9e156943ec199a8892c0f21486f95ac1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaea76a58026a88e256a0efaef712976",
+    "url": "/portfolio/about/index.html"
+  },
+  {
+    "revision": "96f974faf9eb89add4a6",
+    "url": "/portfolio/about/static/js/2.332aaec6.chunk.js"
+  },
+  {
+    "revision": "768248413cf74f1cffab91faed2680c9",
+    "url": "/portfolio/about/static/js/2.332aaec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2f23a36fe046a6034f7",
+    "url": "/portfolio/about/static/js/main.dc737abc.chunk.js"
+  },
+  {
+    "revision": "834842e73a3ad55036f3",
+    "url": "/portfolio/about/static/js/runtime-main.c1da68ea.js"
+  },
+  {
+    "revision": "61c94cde65744673502ad68b56c9a926",
+    "url": "/portfolio/about/static/media/avatar.61c94cde.jpg"
+  },
+  {
+    "revision": "7dcfabfab769ad7d489b429d8a134612",
+    "url": "/portfolio/about/static/media/background.7dcfabfa.jpg"
+  },
+  {
+    "revision": "dc5dd6cb1f9c4c8402752fd13fa44764",
+    "url": "/portfolio/about/static/media/user2.dc5dd6cb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1a63b6ef71d0d8ec070334c894400507.js"
+  "/portfolio/about/precache-manifest.9e156943ec199a8892c0f21486f95ac1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/about/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
